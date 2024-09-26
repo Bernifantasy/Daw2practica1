c8fa478fb5727d30e1d3c1dbc4fae07496a92579
jordipa94 <jpujol6@inscaparrella.cat>
2024-09-26 19:42:31 +0200

    prova3

---
+++ prova3.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+<?php
+
+    function sumar($num1,$num2){
+
+        $suma = $num1 + $num2;
+
+        return $suma;
+
+    }
+
+    echo sumar(1,2);
+
+    ?>
+
+</body>
+</html>
